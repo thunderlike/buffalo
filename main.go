70ae9e91d3bc75c3c72d33e6759487b9b0727d2e
lvzhenyu <lvzhenyu@autohome.com.cn>
2019-10-31 12:13:17 +0800

    init

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/thunderlike/buffalo/logs"
+	"github.com/thunderlike/buffalo/routers"
+	"github.com/thunderlike/buffalo/setting"
+	"log"
+	"net/http"
+)
+
+func init() {
+	setting.Setup()
+	logs.Setup()
+}
+
+func main() {
+	gin.SetMode(setting.ServerSetting.RunMode)
+
+	routersInit := routers.InitRouter()
+	readTimeout := setting.ServerSetting.ReadTimeout
+	writeTimeout := setting.ServerSetting.WriteTimeout
+	endPoint := fmt.Sprintf(":%d", setting.ServerSetting.HttpPort)
+	maxHeaderBytes := 1 << 20
+
+	server := &http.Server{
+		Addr:           endPoint,
+		Handler:        routersInit,
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+		MaxHeaderBytes: maxHeaderBytes,
+	}
+
+	log.Printf("[info] start http server listening %s", endPoint)
+
+	server.ListenAndServe()
+
+	// If you want Graceful Restart, you need a Unix system and download github.com/fvbock/endless
+	//endless.DefaultReadTimeOut = readTimeout
+	//endless.DefaultWriteTimeOut = writeTimeout
+	//endless.DefaultMaxHeaderBytes = maxHeaderBytes
+	//server := endless.NewServer(endPoint, routersInit)
+	//server.BeforeBegin = func(add string) {
+	//	log.Printf("Actual pid is %d", syscall.Getpid())
+	//}
+	//
+	//err := server.ListenAndServe()
+	//if err != nil {
+	//	log.Printf("Server err: %v", err)
+	//}
+}
---
+++ routers/routers.go
@@ -0,0 +1,9 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter() *gin.Engine {
+	return nil
+}
